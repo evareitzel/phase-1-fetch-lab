1b292bfe98dd249fdc6c1f325f99b413bd15f486
Eva Reitzel <evareitzel@gmail.com>
2022-06-05 12:56:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   return fetch('https://anapioficeandfire.com/api/books')
-  .then(res => res.json())
+  .then(resp => resp.json())
+  // .then(data => console.log(data))
   .then(data => renderBooks(data))
 }
 
